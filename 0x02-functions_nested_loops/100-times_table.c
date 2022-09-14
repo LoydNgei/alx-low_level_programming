59512c145e1141e74ac6a9c1377f44fdf91dfea9
loyd ngei <loydngei@gmail.com>
2022-09-13 22:29:25 -0700

    100-times_tables.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table starting with 0
- * @n - number of the times table
+ * @n - integer to be checked
  */
 void print_times_table(int n)
 {
@@ -36,7 +36,7 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-			                _putchar((k / 100) + '0');
+					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
