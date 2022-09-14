b9f9111a75533596b83f9b9950c54f8910671883
loyd ngei <loydngei@gmail.com>
2022-09-14 10:51:10 -0700

    101-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,13 +2,13 @@
 
 /**
  * print_times_table - prints the n times table starting with 0
- * @n - The number to be checked
+ * @n - The number of the times table
  */
 void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n > 15 || n < 0)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -31,7 +31,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
-					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
 					_putchar(',');
