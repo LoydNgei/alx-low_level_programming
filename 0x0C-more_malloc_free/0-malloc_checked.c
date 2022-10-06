3ad08c447f2e6b60fa66b99b790281f9ff27c84b
loyd ngei <loydngei@gmail.com>
2022-10-06 05:14:54 -0700

    allocates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 /**
  * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
- * Return: a pointer to allocated memory(Success)
+ * Return: a pointer to allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
