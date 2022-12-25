df1d6e9754167d1707dd03538e0e82fea2f73856
loyd ngei <loydngei@gmail.com>
2022-12-25 21:42:35 +0300

    Err fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,8 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
- * @h: node of the dlistint_t list
+ * @h: head of the dlistint_t list
  * Return: the number of nodes on success, NULL if it fails
  */
 size_t print_dlistint(const dlistint_t *h)
@@ -16,5 +15,5 @@ size_t print_dlistint(const dlistint_t *h)
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return num;
+	return (num);
 }
